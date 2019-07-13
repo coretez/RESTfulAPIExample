11d3d9f44bd36ddc1e97ebe471bb044f4583dc79
Chris Jordan <chris@fluencysecurity.com>
2019-07-13 13:02:50 -0400

    Changing the git site

---
+++ README.md
@@ -0,0 +1,9 @@
+# RESTful API Example
+The objective of project is it to write a more modern structured project that can be maintained.
+
+This project uses NodeJS and MongoDB. This project aims to have:
+* A complete RESTful API
+* Unit testing of the API
+* OAuth authentication
+
+As we develop this project. This page will reflect the documentation to install, run, maintain and operate it. 
---
+++ booksJson.js
@@ -0,0 +1,32 @@
+db.books.insert([
+  {
+    title: 'War and Peace',
+    genre: 'Histrical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false,
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Histrical Fiction',
+    author: 'Victor Hugo',
+    read: false,
+  },
+  {
+    title: 'Illustrated TCP/IP',
+    genre: 'Computer',
+    author: 'Stevens Wright',
+    read: false,
+  },
+  {
+    title: 'The Waterman',
+    genre: 'History',
+    author: 'Tim Junkin',
+    read: false,
+  },
+  {
+    title: 'No Time for Sargents',
+    genre: 'Fiction',
+    author: 'Mac Hyman',
+    read: false,
+  },
+]);
