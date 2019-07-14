f0cb3f8a3d80aae4ce2bf683c21537523a5c804a
Chris Jordan <chris@fluencysecurity.com>
2019-07-14 06:58:04 -0400

    Final testing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (process.env.ENV === 'Test') {
   console.log('this is a test database');
   const db = mongoose.connect('mongodb://localhost/bookAPI-testdatabase', { useNewUrlParser: true });
 } else {
-  const db = mongoose.connect('mongodb://localhost/bookAPI-prod', { useNewUrlParser: true });
+  const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
 }
 
 const port = process.env.PORT || 3000;
