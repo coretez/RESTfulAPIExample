5b74dd9e5b2f18fe70028c7de2229b11d1b2c4f7
Chris Jordan <chris@fluencysecurity.com>
2019-07-13 21:51:28 -0400

    Added Testing

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
-
 const app = express();
+
+if (process.env.ENV === 'Test') {
+  console.log('this is a test database');
+  const db = mongoose.connect('mongodb://localhost/bookAPI-testdatabase', { useNewUrlParser: true });
+} else {
+  const db = mongoose.connect('mongodb://localhost/bookAPI-prod', { useNewUrlParser: true });
+}
+
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
- 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
- 
+
 app.use('/api', bookRouter);
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Express server started on port: ${port}`);
 });
+
+module.exports = app;
